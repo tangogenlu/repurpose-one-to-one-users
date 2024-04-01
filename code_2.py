13574e23250f3f1710ef34d5a1b677b4e6b99139
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2024-04-01 06:53:03 +0300

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,17 +1,16 @@
-    }
     const data = [];
-        data.push(Math.floor(Math.random() * 100) + 1);
-function generateRandomData() {
-
+        console.log("Random Number: " + item);
+main();
     const data = generateRandomData();
 
-main();
+}
     });
 
-    for (let i = 0; i < 10; i++) {
-function main() {
     data.forEach(item => {
-}
+function main() {
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
     return data;
+    }
 }
-        console.log("Random Number: " + item);
